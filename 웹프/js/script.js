04f23950238d84709544462508f7b953518065c6
hA2zy <128462388+hA2zy@users.noreply.github.com>
2023-11-08 09:19:04 +0900

    웹프 추가

---
+++ 웹프/js/script.js
@@ -0,0 +1,9 @@
+let id = "길동";
+let greetings = "반갑습니다.";
+
+console.log(`홀${id} 너무너무 ${greetings}`);
+console.log(!!1);
+console.log(!!-1);
+console.log(!!"text");
+console.log(!!{});
+console.log(!!Infinity);
---
+++ 웹프/var.js
@@ -0,0 +1,28 @@
+const number = 123;
+
+console.log(number);
+console.log(number.toString());
+
+const text = "text";
+
+console.log(text);
+console.log(text.length);
+text.trim();
+
+console.log(Math.max(1, 2, 3));
+console.log(Math.min(1, 2, 3));
+console.log(3 ** 2);
+console.log(Math.pow(3, 2));
+console.log(Math.random());
+console.log(Math.floor(Math.random() * 10 + 1));
+
+const textObj = new String("Helo World");
+const text2 = "Hello World";
+
+console.log(textObj);
+console.log(text2);
+
+const today = new Date();
+console.log(today);
+console.log(today.getDate());
+console.log(new Date("2025-02-25"));
