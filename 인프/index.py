645f41850ab83ad83fb76b61c87cbe2f302da7b4
hA2zy <haz2y@dsm.hs.kr>
2023-11-10 10:44:19 +0900

    주석

--- 인프/index.py
+++ 인프/index.py
@@ -193,61 +193,62 @@
 #     else:
 #         print("올바른 선택이 아닙니다.")
 
-class Calculator:
-    def add(self, x, y):
-        return x + y
+# class Calculator:
+#     def add(self, x, y):
+#         return x + y
 
-    def subtract(self, x, y):
-        return x - y
+#     def subtract(self, x, y):
+#         return x - y
 
-    def multiply(self, x, y):
-        return x * y
+#     def multiply(self, x, y):
+#         return x * y
 
-    def divide(self, x, y):
-        if y == 0:
-            return "0으로 나눌 수 없습니다."
-        return x / y
+#     def divide(self, x, y):
+#         if y == 0:
+#             return "0으로 나눌 수 없습니다."
+#         return x / y
 
-    def modulo(self, x, y):
-        if y == 0:
-            return "0으로 나눌 수 없습니다."
-        return x % y
+#     def modulo(self, x, y):
+#         if y == 0:
+#             return "0으로 나눌 수 없습니다."
+#         return x % y
 
-calculator = Calculator()
+# calculator = Calculator()
 
-while True:
-    print("연산을 선택하세요.")
-    print("1. 덧셈")
-    print("2. 뺄셈")
-    print("3. 곱셈")
-    print("4. 나눗셈")
-    print("5. 나머지 연산")
-    print("6. 종료")
+# while True:
+#     print("연산을 선택하세요.")
+#     print("1. 덧셈")
+#     print("2. 뺄셈")
+#     print("3. 곱셈")
+#     print("4. 나눗셈")
+#     print("5. 나머지 연산")
+#     print("6. 종료")
     
-    choice = input("선택 (1/2/3/4/5/6): ")
+#     choice = input("선택 (1/2/3/4/5/6): ")
 
-    if choice == '6':
-        break
+#     if choice == '6':
+#         break
 
-    if choice in ('1', '2', '3', '4', '5'):
-        num1 = int(input("첫 번째 정수를 입력하세요: "))
-        num2 = int(input("두 번째 정수를 입력하세요: "))
+#     if choice in ('1', '2', '3', '4', '5'):
+#         num1 = int(input("첫 번째 정수를 입력하세요: "))
+#         num2 = int(input("두 번째 정수를 입력하세요: "))
 
-        if choice == '1':
-            print("결과:", calculator.add(num1, num2))
-        elif choice == '2':
-            print("결과:", calculator.subtract(num1, num2))
-        elif choice == '3':
-            print("결과:", calculator.multiply(num1, num2))
-        elif choice == '4':
-            if num2 == 0:
-                print("0으로 나눌 수 없습니다.")
-            else:
-                print("결과:", calculator.divide(num1, num2))
-        elif choice == '5':
-            if num2 == 0:
-                print("0으로 나눌 수 없습니다.")
-            else:
-                print("결과:", calculator.modulo(num1, num2))
-    else:
-        print("올바른 선택이 아닙니다.")
+#         if choice == '1':
+#             print("결과:", calculator.add(num1, num2))
+#         elif choice == '2':
+#             print("결과:", calculator.subtract(num1, num2))
+#         elif choice == '3':
+#             print("결과:", calculator.multiply(num1, num2))
+#         elif choice == '4':
+#             if num2 == 0:
+#                 print("0으로 나눌 수 없습니다.")
+#             else:
+#                 print("결과:", calculator.divide(num1, num2))
+#         elif choice == '5':
+#             if num2 == 0:
+#                 print("0으로 나눌 수 없습니다.")
+#             else:
+#                 print("결과:", calculator.modulo(num1, num2))
+#     else:
+#         print("올바른 선택이 아닙니다.")
+
