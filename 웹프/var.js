563182aaf993bd8f1a9a510b3e7424545e4b3fc0
hA2zy <128462388+hA2zy@users.noreply.github.com>
2023-11-13 13:36:29 +0900

    연습

--- 웹프/var.js
+++ 웹프/var.js
@@ -1,28 +1,38 @@
-const number = 123;
+// const number = 123;
 
-console.log(number);
-console.log(number.toString());
+// console.log(number);
+// console.log(number.toString());
 
-const text = "text";
+// const text = "text";
 
-console.log(text);
-console.log(text.length);
-text.trim();
+// console.log(text);
+// console.log(text.length);
+// text.trim();
 
-console.log(Math.max(1, 2, 3));
-console.log(Math.min(1, 2, 3));
-console.log(3 ** 2);
-console.log(Math.pow(3, 2));
-console.log(Math.random());
-console.log(Math.floor(Math.random() * 10 + 1));
+// console.log(Math.max(1, 2, 3));
+// console.log(Math.min(1, 2, 3));
+// console.log(3 ** 2);
+// console.log(Math.pow(3, 2));
+// console.log(Math.random());
+// console.log(Math.floor(Math.random() * 10 + 1));
 
-const textObj = new String("Helo World");
-const text2 = "Hello World";
+// const textObj = new String("Helo World");
+// const text2 = "Hello World";
 
-console.log(textObj);
-console.log(text2);
+// console.log(textObj);
+// console.log(text2);
 
-const today = new Date();
-console.log(today);
-console.log(today.getDate());
-console.log(new Date("2025-02-25"));
+// const today = new Date();
+// console.log(today);
+// console.log(today.getDate());
+// console.log(new Date("2025-02-25"));
+
+console.log(a)
+
+function num () {
+    var a = 123;
+    console.log(a)
+}
+
+console.log(num)
+console.log(a)
